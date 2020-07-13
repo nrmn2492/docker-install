1d49ae77e3cde85679683f25f29097944c90186a
htsnorman <htsnorman@users.noreply.github.com>
2020-07-13 14:13:28 +0200

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo 'Docker telepítése'
+echo '########## Docker telepítése ##########'
 sleep 2
 apt install apt-transport-https ca-certificates curl software-properties-common gnupg2 -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update 
 apt install docker-ce -y
-echo 'Docker-compose telepítése'
+echo '########## Docker-compose telepítése ##########'
 sleep 2
 apt-get remove composer
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
