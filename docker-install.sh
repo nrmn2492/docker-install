1bdc8148e456f7cc92cd743b197fce9aa0837aaa
htsnorman <htsnorman@users.noreply.github.com>
2022-01-14 15:33:21 +0100

    updaet docker-compsoe version

--- docker-install.sh
+++ docker-install.sh
@@ -9,7 +9,7 @@ apt install docker-ce -y
 echo '########## Docker-compose telepítése ##########'
 sleep 2
 apt-get remove composer
-curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 echo 'KÉSZ'
